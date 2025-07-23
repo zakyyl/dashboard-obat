916672c7fe53456700d2dfde5bf1742b9f9e3192
zakyyl <zaky24112003@gmail.com>
2025-07-23 23:35:24 +0700

    create route page.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ObatController;
+use App\Http\Controllers\ObatSaatIniController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard/stok-barang', [DashboardController::class, 'stokBarang'])->name('dashboard.stok-barang');
+Route::get('/obat/stok-barang', [ObatController::class, 'stokBarang'])->name('obat.stok-barang');
+Route::get('/obat/search-obat', [ObatController::class, 'searchObat'])->name('obat.search-obat');
+
+Route::get('/obat/obat-saat-ini', [ObatSaatIniController::class, 'index']);
+Route::get('/obat/obat-saat-ini/search', [ObatSaatIniController::class, 'searchObatSaatIni'])->name('obat.search-obat-saat-ini');
+
+
