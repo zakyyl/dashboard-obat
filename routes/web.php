b2a9c8e9039804ae807cf43e6377e86e098679b3
zakyyl <zaky24112003@gmail.com>
2025-07-24 14:37:05 +0700

    delete obatsaatini controller and route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ObatController;
-use App\Http\Controllers\ObatSaatIniController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,8 +10,5 @@ Route::get('/', function () {
 
 Route::get('/obat/stok-barang', [ObatController::class, 'stokBarang'])->name('obat.stok-barang');
 Route::get('/obat/search-obat', [ObatController::class, 'searchObat'])->name('obat.search-obat');
-
-Route::get('/obat/obat-saat-ini', [ObatSaatIniController::class, 'index']);
-Route::get('/obat/obat-saat-ini/search', [ObatSaatIniController::class, 'searchObatSaatIni'])->name('obat.search-obat-saat-ini');
 
 
