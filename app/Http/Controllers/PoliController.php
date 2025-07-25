b0809613a7187304485783b495e7680e9cab65c8
zakyyl <zaky24112003@gmail.com>
2025-07-25 09:58:06 +0700

    create polidokter.

--- app/Http/Controllers/PoliController.php
+++ app/Http/Controllers/PoliController.php
@@ -29,6 +29,21 @@ public function index(Request $request)
 
     return view('dashboard.poli', compact('data', 'tgl_dari', 'tgl_sampai'));
 }
+public function indexPerDokter(Request $request)
+{
+    $tgl_dari = $request->get('tgl_dari', now()->toDateString());
+    $tgl_sampai = $request->get('tgl_sampai', now()->toDateString());
+
+    $data = DB::table('reg_periksa')
+        ->join('dokter', 'reg_periksa.kd_dokter', '=', 'dokter.kd_dokter')
+        ->whereBetween('reg_periksa.tgl_registrasi', [$tgl_dari, $tgl_sampai])
+        ->select('dokter.nm_dokter', DB::raw('COUNT(dokter.nm_dokter) AS jumlah'))
+        ->groupBy('dokter.nm_dokter')
+        ->orderBy('jumlah', 'desc')
+        ->get();
+
+    return view('dashboard.poli_perdokter', compact('data', 'tgl_dari', 'tgl_sampai'));
+}
 
 
 }
